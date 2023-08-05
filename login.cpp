364662aa3ce28cfa01244801004b3933d0149820
jeezitzsurbhi <141508811+jeezitzsurbhi@users.noreply.github.com>
2023-08-05 21:20:44 +0530

    Add files via upload
    for access granted,
    type "surbhi3605" in username column.
    type "lmaoded" in password column.

---
+++ login.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    cout<<"LOGIN DETAILS"<<endl;
+// char surbhi;
+// char SURBHI3605;
+// char abcatew;
+// char name;
+//     char username;
+//     char email_id;
+// string name;
+// string surbhi;
+//     cout<<"\nNAME: ";
+//     cin>>name = "Surbhi";
+//     if(name == surbhi){
+//         string email_id;
+//         string abc;
+//         cout<<"EMAIL ID.: ";
+//         cin>>email_id;
+//         if(email_id == abc){
+            string username;
+            string hehe = "surbhi3605";
+            cout<<"ENTER USERNAME: ";
+            cin>>username;
+            if(username == hehe){
+                string password;
+                string haha = "123456";
+                cout<<"ENTER PASSWORD: ";
+                cin>>password;
+                if(password == haha){
+                    cout<<"ACCESS GRANTED"<<endl;}
+                else{
+                    cout<<"ACCESS DENIED"<<endl;
+                }
+            
+                }
+            else{
+                string password;
+                cout<<"ENTER PASSWORD: ";
+                cin>>password;
+
+
+                cout<<"INVALID USERNAME OR PASSWORD"<<endl;
+            }
+
+        
+    //     }
+    //     else{
+    //             cout<<"ACCESS DENIED"<<endl;
+    //         }
+
+
+    // }
+    // else{
+    //             cout<<"ACCESS DENIED"<<endl;
+    //         }
+    //         return 0;
+   }
